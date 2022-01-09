2a8a440eafe3e61a3df5f0cce26e2e7f0d97c46d
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-09 21:19:19 +0100

    create center controller and copy their functions from admin controller

--- marjane_app/src/controllers/adminController.js
+++ marjane_app/src/controllers/adminController.js
@@ -66,33 +66,4 @@ const getStats = async (req, res) => {
   res.status(200).json({ logs });
 };
 
-const getCenters = async (req, res) => {
-  const centers = await prisma.center
-    .findMany()
-    .catch((e) => {
-      res.status(400).json({
-        error: e.message,
-      });
-    });
-
-}
-const getCentersPromotions = async (req, res) => {
-  const centersPromotions = await prisma.center
-    .findMany({
-      include: {
-        SubAdmin: {
-          select: {
-            id: true,
-            _count: {
-              select: { Promotion: true },
-            },
-          }
-
-        }
-
-      },
-    })
-  res.status(200).json({ centersPromotions });
-}
-
-export { createSubAdmin, loginAdmin, getStats, getCenters, getCentersPromotions };
+export { createSubAdmin, loginAdmin, getStats };
