1fccf5a285b203b573771614908a6eb9090573d8
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-12 20:25:57 +0100

    add new route to check if the subAdmin was auth before render the template

--- marjane_app/src/routes/subAdminRouter.js
+++ marjane_app/src/routes/subAdminRouter.js
@@ -9,11 +9,12 @@ import {
     getCategoriesCenter
 } from "../controllers";
 
-import { Auth } from "../middlewares";
+import { Auth, checkAuth } from "../middlewares";
 
 router.post("/login", loginSubAdmin);
 router.post("/createManager", Auth("SUBADMIN"), createManager);
 router.post("/createPromo", Auth("SUBADMIN"), createPromo);
+router.get("/checkAuth", checkAuth("SUBADMIN")); // Check if autheticated with token
 router.get("/getAllManagerCenter", Auth("SUBADMIN"), idFromToken("SUBADMIN"), getAllManagerCenter); // Bring all manager to the center of this subAdmin
 router.get("/getCategoriesCenter", Auth("SUBADMIN"), idFromToken("SUBADMIN"), getCategoriesCenter); // Bring all categories to the center of this subAdmin
 
